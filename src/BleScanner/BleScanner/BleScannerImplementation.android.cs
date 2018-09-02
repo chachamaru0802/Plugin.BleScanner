a6c1a2817753a4be439c2a341cdc24a0a8f2b532
chachamaru0802 <chachamaru.0802.k@gmail.com>
2018-09-02 13:21:56 +0900

    プロジェクト 新規作成

---
+++ src/BleScanner/BleScanner/BleScannerImplementation.android.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.BleScanner
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class BleScannerImplementation : IBleScanner
+    {
+    }
+}
