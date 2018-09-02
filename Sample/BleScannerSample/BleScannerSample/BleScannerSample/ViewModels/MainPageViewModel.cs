36fb0f85ed9f02bf8017588f8639139df1a9cd40
chachamaru0802 <chachamaru.0802.k@gmail.com>
2018-09-03 06:38:30 +0900

    サンプルプロジェクト 修正

--- Sample/BleScannerSample/BleScannerSample/BleScannerSample/ViewModels/MainPageViewModel.cs
+++ Sample/BleScannerSample/BleScannerSample/BleScannerSample/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BleScannerSample.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public DelegateCommand BleScanCommand => new DelegateCommand(async () => await BleScan());
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+        }
+
+        Task BleScan()
+        {
+            var result = Plugin.BleScanner.CrossBleScanner.Current.Scan();
+
+
+            return Task.CompletedTask;
         }
     }
 }
